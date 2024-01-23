68a1a4f76dd2f477143d468d740e2bf59ece61c7
krystalpothilat <krystalpothilat@gmail.com>
2024-01-22 18:37:15 -0800

    formatting for screen/window sizes

--- src/Page.js
+++ src/Page.js
@@ -12,12 +12,12 @@ function Page({ id, img, about1, section, info, logo }) {
                 <img src = {img} alt ="" className = "pic"/>
             </div>
             <div id = "mainintro">
-                <h2> hello! I'm Krystal Pothilat</h2>
-                <ul className = "cookiepoints">
+                <h2 id = "main"> hello! I'm Krystal Pothilat</h2>
+                <ul className = "text" id = "cookiepoints">
                     <li> San Diego, CA</li>
                     <li> B.S. in Computer Science at UC Riverside</li>
                 </ul>
-                </div>
+            </div>
         </div>  
         ) : (
             <div className = "infopage">
@@ -31,9 +31,9 @@ function Page({ id, img, about1, section, info, logo }) {
                     {Array.isArray(info) && info.map((item) => (
                         <div className = "info-container">
                             <h2 id = "title"> {item.title}</h2>
-                            <p id = "desc"> {item.description} </p>
+                            <p className = "text"> {item.description} </p>
                             {item.bulletpoints && (
-                            <ul>
+                            <ul className = "text">
                                 {item.bulletpoints.map((bulletpoints, index) => (
                                     <li key={index}>{bulletpoints}</li>
                                 ))}
@@ -45,34 +45,6 @@ function Page({ id, img, about1, section, info, logo }) {
             </div>
         )}
         
-         {/* <div className = "header-container">
-            <div className = "circle">
-                <img src={mainCookie} alt="" className ="pic" />
-            </div>
-                <p className = "header">{section}</p>
-        </div>
-    
-        <div className = "section-info">
-            {about1 && 
-                <div id = "mainintro">
-                    <h2> hello! I'm Krystal Pothilat</h2>
-                    <ul className = "cookiepoints">
-                        <li> San Diego, CA</li>
-                        <li> B.S. in Computer Science at UC Riverside</li>
-                    </ul>
-                
-                </div>  
-            }
-
-            
-            {logo && <img src={logo} alt="Logo" className="logo" />}
-
-            {Array.isArray(info) ? (
-                <List data={info} />
-            ) : (
-                <p className="additional-info">{info}</p>
-            )}
-        </div>  */}
         
     </div>
   );
